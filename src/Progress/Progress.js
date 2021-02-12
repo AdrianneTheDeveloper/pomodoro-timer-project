a18b4aaa031e7b9c173ee42985925612f4d0f118
AdrianneTheDeveloper <meredith.adri@gmail.com>
2021-02-11 18:25:49 -0600

    Updated Progress

--- src/Progress/Progress.js
+++ src/Progress/Progress.js
@@ -1,62 +1,119 @@
-import React, { useState } from "react";
-import classNames from "../utils/class-names";
-import useInterval from "../utils/useInterval";
-import { secondsToDuration } from "../utils/duration";
-import FocusDurationChange from "../Focus/FocusDuration";
-import BreakDurationChange from "../Break/BreakDuration";
+import React from "react";
+import { secondsToDuration, minutesToDuration } from "../utils/duration";
 
-function Progress({ isTimerRunning,
-    playPause,
-    focusDuration,
-    timeRemaining,
-    toFocus,
-    setToFocus,
-    breakDuration,
-    toBreak,
-    setToBreak,
-    elapsedTime,
-    setElapsedTime
+
+function Progress({
+  isTimerRunning,
+  focusDuration,
+  timeRemaining,
+  toFocus,
+  breakDuration,
+  toBreak,
+  elapsedTime,
+  pause,
+  stop,
+  progress
 }) {
-    let timeToAlter = (focusDuration * 60) / 10;
-    useInterval(
-      () => {
-        setElapsedTime((prevTime) => {
-          return prevTime + 10;
-        });
-      },
-      isTimerRunning ? timeToAlter * 1000 : null
+
+  if (toFocus === true && isTimerRunning) {
+    return (
+      <>
+        <div className="row mb-2">
+          <div className="col">
+            {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
+            <h2 data-testid="session-title">
+              Focusing for {minutesToDuration(focusDuration)} minutes
+            </h2>
+            {/* TODO: Update message below to include time remaining in the current session */}
+            <p className="lead" data-testid="session-sub-title">
+              {secondsToDuration(timeRemaining)} remaining
+            </p>
+          </div>
+        </div>
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: "20px" }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={elapsedTime} // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width: `${elapsedTime}%` }} // TODO: Increase width % as elapsed time increases
+              />
+            </div>
+          </div>
+        </div>
+      </>
     );
-    if (isTimerRunning) {
-        return (
-            <>
-            <div className="row mb-2">
-              <div className="col">
-                {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
-                <h2 data-testid="session-title">
-                  Focusing for {focusDuration} minutes
-                </h2>
-                {/* TODO: Update message below to include time remaining in the current session */}
-                <p className="lead" data-testid="session-sub-title">
-                  {secondsToDuration(timeRemaining)} remaining
-                </p>
-              </div>
+  } else if (toBreak === true && isTimerRunning) {
+    return (
+      <>
+        <div className="row mb-2">
+          <div className="col">
+            {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
+            <h2 data-testid="session-title">
+              On Break for {secondsToDuration(breakDuration)} minutes
+            </h2>
+            {/* TODO: Update message below to include time remaining in the current session */}
+            <p className="lead" data-testid="session-sub-title">
+              {secondsToDuration(timeRemaining)} remaining
+            </p>
+          </div>
+        </div>
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: "20px" }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={elapsedTime} // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width: `${elapsedTime}%` }} // TODO: Increase width % as elapsed time increases
+              />
             </div>
-            <div className="row mb-2">
-              <div className="col">
-                <div className="progress" style={{ height: "20px" }}>
-                  <div
-                    className="progress-bar"
-                    role="progressbar"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                    aria-valuenow={elapsedTime} // TODO: Increase aria-valuenow as elapsed time increases
-                    style={{ width: `${elapsedTime}%` }} // TODO: Increase width % as elapsed time increases
-                  />
-                </div>
-              </div>
+          </div>
+        </div>
+      </>
+    );
+  } else if (pause === true && !isTimerRunning) {
+    return (
+      <>
+        <div className="row mb-2">
+          <div className="col">
+            {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
+            <h2 data-testid="session-title">
+              {toFocus === true
+                ? `Focusing for ${minutesToDuration(focusDuration)} minutes`
+                : `On Break for ${secondsToDuration(breakDuration)} minutes`}
+            </h2>
+            {/* TODO: Update message below to include time remaining in the current session */}
+            <p className="lead" data-testid="session-sub-title">
+              {secondsToDuration(timeRemaining)} remaining
+            </p>
+            <h3>PAUSED</h3>
+          </div>
+        </div>
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: "20px" }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={elapsedTime} // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width: `${elapsedTime}%` }} // TODO: Increase width % as elapsed time increases
+              />
             </div>
-          </> 
-       )
-   } return null
+          </div>
+        </div>
+      </>
+    );
+  } else if (stop === true) {
+    return null;
+  }
+  return null;
 }
 export default Progress;
