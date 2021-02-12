f69436a8e2fdf80867a1cddd358f7c9dad746ce2
AdrianneTheDeveloper <meredith.adri@gmail.com>
2021-02-11 18:37:06 -0600

    Added build folder.

---
+++ build/precache-manifest.1bbfed61bd443627cb84354f1669bdd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9074ba719fbe75b659b075c7278d2b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c34a5a0abbe083350931",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34a5a0abbe083350931",
+    "url": "/static/js/main.615b0c5a.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
